41b258afb1a194829778a81b5226762fe3e87ece
Natalia Czuryło <87358@office365.ukw.edu.pl>
2021-05-09 20:16:38 +0200

    Maly update opisow

--- SklepUKW/Infrastructure/FilmsDynamicNodeProvider.cs
+++ SklepUKW/Infrastructure/FilmsDynamicNodeProvider.cs
@@ -10,25 +10,28 @@ namespace SklepUKW.Infrastructure
     public class FilmsDynamicNodeProvider : DynamicNodeProviderBase
     {
         FilmsContext db = new FilmsContext();
-
         public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
         {
             var filmsDN = new List<DynamicNode>();
+            //przeszukiwanie bazy danych pod katem  filmow
+            //dostep do bazy danych w kontrolerach poprzez klase films context
 
             foreach (var film in db.Films)
             {
                 DynamicNode dn = new DynamicNode()
                 {
-                    Title = film.Title,
-                    Key = "Film_" + film.FilmId,
-                    ParentKey = "Category_" + film.CategoryId
+                    Title = film.Title,//tytul dynamic node. to sa parametry dla node z Mvc.sitemap
+                    Key = "Film_" + film.FilmId, //nazwa wlasna filmu
+                    ParentKey = "Category_" + film.CategoryId//rodzicem filmu jest kategoria. w filmie jest klucz obcy idkategorii categoryid
                 };
-                dn.RouteValues.Add("id", film.FilmId);
+                dn.RouteValues.Add("id", film.FilmId); //dla akcji details przyjmujemy id konkretnego filmu
 
-                filmsDN.Add(dn);
+                //node trzeba wrzucic do listy node'ow
+                filmsDN.Add(dn); //wrzucamy tego node
             }
 
-            return filmsDN;
+            //po wyjsciu z foreach trzeba zwrocic
+            return filmsDN; //node fla Filmow. jeszcze node dla kategorii
         }
     }
 }
