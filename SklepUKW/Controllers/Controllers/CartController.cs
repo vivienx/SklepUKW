2c9a1b71307970adb5390b786da4bbe6dd5c1a96
Natalia Czuryło <87358@office365.ukw.edu.pl>
2021-06-01 11:40:50 +0200

    Finishing touches

---
+++ SklepUKW/Controllers/Controllers/CartController.cs
@@ -0,0 +1,63 @@
+﻿using SklepUKW.DAL;
+using SklepUKW.Infrastructure;
+using SklepUKW.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SklepUKW.Controllers
+{
+    public class CartController : Controller
+    {
+        CartManager cartManager;
+        FilmsContext db;
+        ISessionManager session;
+
+        public CartController()
+        {
+            db = new FilmsContext();
+            session = new SessionManager();
+            cartManager = new CartManager(db, session);
+        }
+
+
+        // GET: Cart
+        public ActionResult Index()
+        {
+            CartViewModel cvm = new CartViewModel()
+            {
+                CartItems = cartManager.GetCartItems(),
+                TotalPrice = cartManager.GetCartValue()
+            };
+
+            return View(cvm);
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            cartManager.AddToCart(id);
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult RemoveFromCart(int id)
+        {
+            ItemRemoveViewModel model = new ItemRemoveViewModel()
+            {
+                ItemId = id,
+                ItemQuantity = cartManager.RemoveFromCart(id),
+                CartValue = cartManager.GetCartValue(),
+                CartQuantity = cartManager.GetCartQuantity()
+            };
+
+            return Json(model);
+        }
+
+        public int GetCartQuantity()
+        {
+            return cartManager.GetCartQuantity();
+        }
+    }
+}
---
+++ SklepUKW/Controllers/DAL/FilmsContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using SklepUKW.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SklepUKW.DAL
+{
+    public class FilmsContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Film> Films { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public FilmsContext() : base("FilmsContext")
+        {
+
+        }
+
+        static FilmsContext()
+        {
+            Database.SetInitializer<FilmsContext>(new FilmsInitializer());
+        }
+
+        public static FilmsContext Create()
+        {
+            return new FilmsContext();
+        }
+    }
+}
